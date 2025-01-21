d62927ca395e8e6cde9604e45e8afbecc0fe4856
JT Smith <jt@plainblack.com>
2025-01-21 12:24:43 -0600

    remove undefined tables

--- ving/drizzle/map.mjs
+++ ving/drizzle/map.mjs
@@ -1,22 +1,10 @@
 import { APIKeyTable } from "#ving/drizzle/schema/APIKey.mjs";
 import { UserTable } from "#ving/drizzle/schema/User.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
-import { undefinedTable } from "#ving/drizzle/schema/undefined.mjs";
 import { CronJobTable } from "#ving/drizzle/schema/CronJob.mjs";
 import { S3FileTable } from "#ving/drizzle/schema/S3File.mjs";
 import { OrderTable } from "#ving/drizzle/schema/Order.mjs";
 export const tableModules = {
     User: UserTable,
-    undefined: undefinedTable,
-    undefined: undefinedTable,
-    undefined: undefinedTable,
-    undefined: undefinedTable,
-    undefined: undefinedTable,
-    undefined: undefinedTable,
     APIKey: APIKeyTable,
     CronJob: CronJobTable,
     S3File: S3FileTable,
